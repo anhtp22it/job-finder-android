d8520adfdf5cf8500983757ee234abc8fcbf1dcd
Trần Phước Anh <anhtp.22it@vku.udn.vn>
2024-05-04 23:17:29 +0700

    add search no result

--- app/src/main/java/com/tpanh/jobfinder/screens/SearchResult.kt
+++ app/src/main/java/com/tpanh/jobfinder/screens/SearchResult.kt
@@ -1,17 +1,21 @@
 package com.tpanh.jobfinder.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
@@ -19,13 +23,16 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.tpanh.jobfinder.R
 import com.tpanh.jobfinder.screens.components.JobItem
 import com.tpanh.jobfinder.screens.components.NavigateBackBar
 import com.tpanh.jobfinder.viewmodel.SearchViewModel
@@ -93,10 +100,41 @@ fun SearchResultContent(
                 }
             }
         )
-        Spacer(modifier = Modifier.height(16.dp))
-        repeat(10) {
-            JobItem()
-            Spacer(modifier = Modifier.height(8.dp))
+        if (false) {
+            Spacer(modifier = Modifier.height(16.dp))
+            repeat(10) {
+                JobItem()
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+        } else {
+            Column (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(32.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(painter = painterResource(
+                    id = R.drawable.img_no_result),
+                    contentDescription = "no result",
+                    modifier = Modifier.size(200.dp)
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                Text(
+                    text = "No results found",
+                    fontSize = 20.sp,
+                    textAlign = androidx.compose.ui.text.style.TextAlign.Center,
+                    fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = "The search could not be found, please check spelling or write another word",
+                    textAlign = androidx.compose.ui.text.style.TextAlign.Center,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+
+            }
         }
     }
 }
